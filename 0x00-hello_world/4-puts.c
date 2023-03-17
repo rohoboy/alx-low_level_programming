b145694cb83f37c77526103796bdedae1eb1a7b1
rohoboy <petergicheha39@gmail.com>
2023-03-16 19:46:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
+	/**
+	 * main program that prints programming is like building ....
+	 * Return 0 if it is successfull
+	*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
