0ba46bbde004736c690d1ddacb126f1c4c5f1a6f
rohoboy <petergicheha39@gmail.com>
2023-03-25 06:42:13 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _islower - Entry point
- * Return: 1
+ *_islower - Entry point for this function
+ *@c: the character to be checked
+ * Return: 1 for lower character or 0 for other
  */
 
 int _islower(int c)
