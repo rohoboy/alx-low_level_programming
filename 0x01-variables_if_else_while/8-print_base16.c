476cbca1bbcf70337dabd7468a2a541a1a59fdda
rohoboy <petergicheha39@gmail.com>
2023-03-25 00:18:28 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - a program to print the base 16.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j = 97;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 102)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
