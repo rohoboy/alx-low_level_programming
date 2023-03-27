5475cdc653562892762952ff42a75b3ad860d24d
rohoboy <petergicheha39@gmail.com>
2023-03-26 23:51:04 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - Entry point
+ *@n: The number to be checked
+ *Return: 0 for positive, -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
