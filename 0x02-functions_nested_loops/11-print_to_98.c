62d7c9230aad869bba5466a262bbcc03519400d3
rohoboy <petergicheha39@gmail.com>
2023-03-29 00:54:32 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry point
+ * @n: this one
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d ", n);
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d ", n);
+			}
+			n--;
+		}
+	}
+}
+
