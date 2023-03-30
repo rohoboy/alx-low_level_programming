a5db7a0bcd91f872fa8c51513ec26e8f762e5631
rohoboy <petergicheha39@gmail.com>
2023-03-30 05:35:48 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string -Entry point
+ * @n: string
+ * Return: string
+ */
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i])
+	{
+		while (!(n[i] >= 'a' && n[i] <= 'z'))
+			i++;
+		if (n[i] == ' ' ||
+		n[i] == '\t' ||
+		n[i] == '\n' ||
+		n[i] == ',' ||
+		n[i] == '.' ||
+		n[i] == ';' ||
+		n[i] == '?' ||
+		n[i] == '!' ||
+		n[i] == '"' ||
+		n[i] == '(' ||
+		n[i] == ')' ||
+		n[i] == '{' ||
+		n[i] == '}' ||
+		i  == 0)
+			n[i] -= 32;
+	}
+	return (n);
+}
