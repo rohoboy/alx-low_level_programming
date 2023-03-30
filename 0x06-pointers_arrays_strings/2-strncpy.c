7899027d12525d09e643be7e31b8e1345a45dc18
rohoboy <petergicheha39@gmail.com>
2023-03-29 19:34:08 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy -Entry point
+ * @dest: destination
+ * @src: source
+ * @n: length
+ * Return: copy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
