34cebb4bcb3e7bf4514df7b56a2857bfb39f845b
rohoboy <petergicheha39@gmail.com>
2023-03-29 18:17:45 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strcat - Entry point
+ * @dest: par 1
+ * @src:  par 2
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\n';
+	return (dest);
+}
