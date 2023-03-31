c086135c46ff0d55f3134b8e66e209f770db906a
rohoboy <petergicheha39@gmail.com>
2023-03-30 18:53:05 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - Function
+ * @str: string
+ * Return: string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
