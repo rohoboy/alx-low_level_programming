e9c15c18642dbd269477855a106d3209157d8759
rohoboy <petergicheha39@gmail.com>
2023-04-01 02:32:11 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper -Entry point
+ * @c: parameter
+ * Return: integer
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
