d12fd849a3e9c4467afb0d4385845eb4525338a6
rohoboy <petergicheha39@gmail.com>
2023-04-17 03:30:05 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
 /**
  * main - print the name of the file
  * Return: 0
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
-
-#endif
