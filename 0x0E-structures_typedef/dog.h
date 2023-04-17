f5e010d7a876cd8cf2ff6529bc2b676bdc6ef94d
rohoboy <petergicheha39@gmail.com>
2023-04-17 04:15:24 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,22 @@
-#include<stdio.h>
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#include <stdio.h>
+/**
+ * struct dog - structure dog
+ * @name: name of the dog
+ * @age: The age of the dog
+ * @owner: Who owns the  dog
+ *
+ * Description: more description goes here
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+
+#endif
