4996c0131bf349e1aa37250242f6772b5293c57c
rohoboy <petergicheha39@gmail.com>
2023-04-20 08:35:03 -0700

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - main function
+ * @argc: accept urguments
+ * @argv: number of argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	operator = get_op_func(argv[2]);
+
+	if (!operator)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add -function to add
+ * @a: first number
+ * @b: second number
+ * Return: answer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub -function to add
+ * @a: first number
+ * @b: second number
+ * Return: answer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul -function to add
+ * @a: first number
+ * @b: second number
+ * Return: answer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div -function to add
+ * @a: first number
+ * @b: second number
+ * Return: answer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod -function to add
+ * @a: first number
+ * @b: second number
+ * Return: answer
+ */
+int op_mod(int a, int b)
+{
+	return (a + b);
+}
