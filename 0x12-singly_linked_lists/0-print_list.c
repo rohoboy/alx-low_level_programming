c9351517609201f6b74e1d0415bdd31d02ac80bb
rohoboy <petergicheha39@gmail.com>
2023-04-28 00:30:23 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,9 +11,9 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
+		printf("[%u] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
 		h = h->next;
 		count++;
 	}
