8a5a2c5496c6dc4980356145d71c1021de1a6c5d
rohoboy <petergicheha39@gmail.com>
2023-04-30 10:50:33 -0700

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - a function to free the linked list
+ * @head: node
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while  (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
+
